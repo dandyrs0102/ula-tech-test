55a7499076ebe1320159139509a3c9299b1c91d8
dandyrs0102 <drive1.rs@gmail.com>
2021-11-08 13:20:48 +0700

    update sign in test

--- cypress/integration/01-web-amazon-test.signin.spec.js
+++ cypress/integration/01-web-amazon-test.signin.spec.js
@@ -1,11 +1,26 @@
 const { BASE_URL, login_email, login_password } = require('../../config')
 
-describe('Verify login amazon web', () => {
-	let badChars = 'Enter the characters as they are given in the challenge.'
+describe('Verify sign in amazon web', () => {
+	let errorBadChars = 'Enter the characters as they are given in the challenge.'
+	let errorSignIn = 'Enter your email or mobile phone number'
 
-	before(function () {
+	beforeEach(function () {
 		cy.visit(BASE_URL)
 		cy.get('#nav-link-accountList').click()
+	})
+	afterEach(function () {
+		cy.reload()
+		cy.clearCookies()
+	})
+
+	it('login with blank email/phone number', () => {
+		cy.get('input[name="email"]').invoke('val', '')
+		cy.get('#continue').click()
+
+		//validate error message
+		cy
+			.get('#auth-email-missing-alert')
+			.should('contain.text', errorSignIn)
 	})
 
 	it('login with invalid captcha characters', () => {
@@ -24,6 +39,6 @@ describe('Verify login amazon web', () => {
 		cy.get('#signInSubmit').click()
 
 		//validate error message
-		cy.get('li').should('be.visible').and('contain.text', badChars)
+		cy.get('li').should('be.visible').and('contain.text', errorBadChars)
 	})
 })
